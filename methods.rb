ad36c4c3e03b91085dbcdc2aaed89d374c1478f0
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-16 21:08:54 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2, 5)
+
+def halve(num1)
+    unless num1.is_a? Integer
+        return nil
+    end
+    
+    num1 / 2
+end
+
+halve(12)
